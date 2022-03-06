fe637e1b5d208adc075ec6f02ce3ffb494c8814b
hansmuc <hans_muc@gmx.de>
2022-03-06 20:55:49 +0100

    dd

--- src/clientReceiver/DisplayReceiver.java
+++ src/clientReceiver/DisplayReceiver.java
@@ -1,6 +1,9 @@
 package clientReceiver;
 
-import java.io.BufferedReader;
+
+
+import java.io.BufferedReader;     
+
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
